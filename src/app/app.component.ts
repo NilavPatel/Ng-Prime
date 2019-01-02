fbbe6e18358395af4d2c34fd0d7e60b3dd4faabd
Nilav Patel <theonetech108@theonetech.local>
2019-01-02 19:31:28 +0530

    runtime theme chooser implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,24 @@ import { LoaderService } from './services/loader.service';
 export class AppComponent implements OnInit {
   title = 'Ng-Prime';
   showLoader: boolean;
+  isDarkTheme: boolean;
 
-  constructor(
-    private loaderService: LoaderService) {
+  constructor(private loaderService: LoaderService) {
+    this.isDarkTheme = true;
   }
 
   ngOnInit() {
     this.loaderService.status.subscribe((val: boolean) => {
       this.showLoader = val;
     });
+  }
+
+  selectTheme(theme) {
+    if (theme === 'dark') {
+      this.isDarkTheme = true;
+    }
+    else {
+      this.isDarkTheme = false;
+    }
   }
 }
